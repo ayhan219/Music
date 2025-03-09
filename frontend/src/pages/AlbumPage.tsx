ebefe8230555d87ca480fddd2e16df31f9d0ed20
Ayhan <ayhan-games@hotmail.com>
2025-03-09 04:32:30 +0300

    albumpage updated for loading effect

--- frontend/src/pages/AlbumPage.tsx
+++ frontend/src/pages/AlbumPage.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import SearchbarArea from "../components/SearchbarArea";
 import { ImMusic } from "react-icons/im";
 import Music from "../components/Music";
@@ -8,39 +8,40 @@ import { useDispatch, useSelector } from "react-redux";
 import { setAlbumMusic } from "../features/MusicSlice";
 import { RootState } from "../app/store";
 
-
-interface AlbumMusic{
-    artist:{
-      name:string
-    },
-    album:{
-      cover_medium:string
-    }
-    duration:number,
-    id:number,
-    md5_image:string,
-    preview:string,
-    rank:number,
-    title:string,
-
+interface AlbumMusic {
+  artist: {
+    name: string;
+  };
+  album: {
+    cover_medium: string;
+  };
+  duration: number;
+  id: number;
+  md5_image: string;
+  preview: string;
+  rank: number;
+  title: string;
 }
 
 const AlbumPage = () => {
   const { id } = useParams();
-  const albumMusic = useSelector((state:RootState)=>state.albumMusic.albumMusic)
+  const albumMusic = useSelector(
+    (state: RootState) => state.albumMusic.albumMusic
+  );
   const dispatch = useDispatch();
-  
-
+  const [loading, setLoading] = useState<boolean>(false);
 
   const getDataFromId = async () => {
+    setLoading(true);
     try {
       const response = await axios.get(
         `https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/${id}`
       );
-      console.log(response.data);
-      dispatch(setAlbumMusic(response.data))
+      dispatch(setAlbumMusic(response.data));
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -50,38 +51,55 @@ const AlbumPage = () => {
 
   return (
     <>
-    <div className="w-full h-screen bg-primary">
-      <SearchbarArea />
-      <div className="w-full h-auto p-5 flex ">
-        <div className="w-full h-[250px] flex  bg-stone-800  items-center gap-4 px-10 rounded-lg  relative ">
-          <img className="w-[200px] h-[200px]" src={albumMusic?.cover_xl}  alt="" />
-          <div className="text-primary font-bold font-mono flex flex-col gap-4">
-            <h3>Open For Public</h3>
-            <h1 className="text-3xl">{albumMusic?.title}</h1>
-            <div>
-            <p>Ayhan - {albumMusic?.tracks?.data.length} songs duration: {albumMusic?.duration ? (albumMusic.duration / 60).toFixed(0) : 0} hours</p>
+      <div className="w-full h-screen bg-primary">
+        <SearchbarArea />
+        {!loading ? (
+          <>
+            <div className="w-full h-auto p-5 flex ">
+              <div className="w-full h-[250px] flex  bg-stone-800  items-center gap-4 px-10 rounded-lg  relative ">
+                <img
+                  className="w-[200px] h-[200px]"
+                  src={albumMusic?.cover_xl}
+                  alt=""
+                />
+                <div className="text-primary font-bold font-mono flex flex-col gap-4">
+                  <h3>Open For Public</h3>
+                  <h1 className="text-3xl">{albumMusic?.title}</h1>
+                  <div>
+                    <p>
+                      Ayhan - {albumMusic?.tracks?.data.length} songs duration:{" "}
+                      {albumMusic?.duration
+                        ? (albumMusic.duration / 60).toFixed(0)
+                        : 0}{" "}
+                      hours
+                    </p>
+                  </div>
+                </div>
+                <div className="text-primary text-7xl">
+                  <ImMusic />
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="text-primary text-7xl">
-            <ImMusic />
-          </div>
-        </div>
-      </div>
-      <div className="px-16 pt-4">
-          <div className="w-full border-b border-gray-600">
-          <h1 className="text-primary font-bold font-mono text-xl">Musics</h1>
-          </div>
-        </div>
+            <div className="px-16 pt-4">
+              <div className="w-full border-b border-gray-600">
+                <h1 className="text-primary font-bold font-mono text-xl">
+                  Musics
+                </h1>
+              </div>
+            </div>
 
-      <div className="w-full h-[500px] px-14 pt-10 flex flex-col gap-3 overflow-y-auto scrollbar-hidden scrollbar-custom">
-
-        {
-          albumMusic?.tracks?.data.map((item:AlbumMusic)=>(
-            <Music item={item}  />
-          ))
-        }
+            <div className="w-full h-[500px] px-14 pt-10 flex flex-col gap-3 overflow-y-auto scrollbar-hidden scrollbar-custom">
+              {albumMusic?.tracks?.data.map((item: AlbumMusic) => (
+                <Music item={item} />
+              ))}
+            </div>
+          </>
+        ) : (
+          <div className="flex justify-center items-center space-x-2">
+            <div className="w-12 h-12 border-4 border-t-transparent border-blue-500 border-solid rounded-full animate-spin"></div>
+          </div>
+        )}
       </div>
-    </div>
     </>
   );
 };
