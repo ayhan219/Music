bb2d81f8cc2e2c300acd4b951ab803e9415c1c4c
Ayhan <ayhan-games@hotmail.com>
2025-03-13 00:02:49 +0300

    added createasyncthunk functinonality to musicslice

--- frontend/src/features/MusicSlice.ts
+++ frontend/src/features/MusicSlice.ts
@@ -1,54 +1,142 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import axios from "axios";
+
+interface ArtistData {
+  artist: {
+    id: number;
+    name: string;
+    picture_big: string;
+  };
+}
+interface AlbumData {
+  cover: string;
+  cover_xl: string;
+  artist: {
+    name: string;
+  };
+  id: number;
+  link: string;
+  position: number;
+  type: string;
+  title: string;
+}
 
 interface AlbumMusic {
-    artist: {
-      name: string;
-    };
-    album: {
-      cover_medium: string;
-    };
-    duration: number;
-    id: number;
-    md5_image: string;
-    preview: string;
-    rank: number;
-    title: string;
-  }
-  
-  interface AlbumMusicState {
-    albumMusic: {
-      title?: string;
-      cover_xl?: string;
-      duration: number;
-      label: string;
-      release_date: string;
-      tracks?: { data: AlbumMusic[] };
-    } | null,
-    currentMusicAlbum:AlbumMusic[]
-    searchedMusicData:AlbumMusic[]
-  }
-
-  const initialState: AlbumMusicState = {
-    albumMusic: null,
-    currentMusicAlbum:[],
-    searchedMusicData:[]
+  artist: {
+    name: string;
   };
+  album: {
+    cover_medium: string;
+  };
+  duration: number;
+  id: number;
+  md5_image: string;
+  preview: string;
+  rank: number;
+  title: string;
+}
 
-  const albumMusicSlice = createSlice({
-    name:"albumMusic",
-    initialState,
-    reducers:{
-        setAlbumMusic: (state, action: PayloadAction<AlbumMusicState['albumMusic']>) => {
-            state.albumMusic = action.payload;
-          },
-          setCurrentAlbumMusic:(state,action: PayloadAction<AlbumMusic[]>)=>{
-            state.currentMusicAlbum= action.payload;
-          },
-          setSearchedMusicData:(state,action: PayloadAction<AlbumMusic[]>)=>{
-            state.searchedMusicData= action.payload;
-          }
+interface AlbumMusicState {
+  albumMusic: {
+    title?: string;
+    cover_xl?: string;
+    duration: number;
+    label: string;
+    release_date: string;
+    tracks?: { data: AlbumMusic[] };
+  } | null;
+  currentMusicAlbum: AlbumMusic[];
+  searchedMusicData: AlbumMusic[];
+  popularAlbums: AlbumData[];
+  loading: boolean;
+  error: string;
+  artistData: ArtistData[];
+}
+
+const initialState: AlbumMusicState = {
+  albumMusic: null,
+  currentMusicAlbum: [],
+  searchedMusicData: [],
+  loading: false,
+  error: "",
+  popularAlbums: [],
+  artistData: [],
+};
+
+export const getPopularAlbums = createAsyncThunk(
+  "albumMusic/getPopularAlbums",
+  async () => {
+    try {
+      const response = await axios.get(
+        "https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/albums"
+      );
+      return response.data.data;
+    } catch (error) {
+      console.log(error);
     }
-  })
+  }
+);
 
-  export const { setAlbumMusic,setCurrentAlbumMusic,setSearchedMusicData } = albumMusicSlice.actions;
+export const getPopularArtist = createAsyncThunk(
+  "albumMusic/getPopularArtists",
+  async () => {
+    try {
+      const response = await axios.get(
+        "https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/artists"
+      );
+      return response.data.tracks.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+const albumMusicSlice = createSlice({
+  name: "albumMusic",
+  initialState,
+  reducers: {
+    setAlbumMusic: (
+      state,
+      action: PayloadAction<AlbumMusicState["albumMusic"]>
+    ) => {
+      state.albumMusic = action.payload;
+    },
+    setCurrentAlbumMusic: (state, action: PayloadAction<AlbumMusic[]>) => {
+      state.currentMusicAlbum = action.payload;
+    },
+    setSearchedMusicData: (state, action: PayloadAction<AlbumMusic[]>) => {
+      state.searchedMusicData = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getPopularAlbums.pending, (state) => {
+        state.loading = true;
+        state.error = "";
+      })
+      .addCase(getPopularAlbums.fulfilled, (state, action) => {
+        state.loading = false;
+        state.popularAlbums = action.payload || [];
+      })
+      .addCase(getPopularAlbums.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message || "Failed to fetch albums";
+      })
+      .addCase(getPopularArtist.pending, (state) => {
+        state.loading = true;
+        state.error = "";
+      })
+      .addCase(getPopularArtist.fulfilled, (state, action) => {
+        state.loading = false;
+        state.artistData = action.payload || [];
+      })
+      .addCase(getPopularArtist.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message || "Failed to fetch artist";
+      });
+  },
+});
+
+export const { setAlbumMusic, setCurrentAlbumMusic, setSearchedMusicData } =
+  albumMusicSlice.actions;
 export default albumMusicSlice.reducer;
