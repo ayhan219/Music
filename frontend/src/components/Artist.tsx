ce3a90ddced37ebffe862584d839c2f2fcb2e882
Ayhan <ayhan-games@hotmail.com>
2025-03-13 00:01:37 +0300

    artist component created and updated

---
+++ frontend/src/components/Artist.tsx
@@ -0,0 +1,27 @@
+
+interface ArtistProps {
+  item:{
+      artist:{
+        id:number,
+        name:string,
+        picture_big:string,
+      }
+  },
+  index:number
+}
+
+const Artist = ({item,index}:ArtistProps) => {
+  return (
+    <div key={index} className="w-[250px] h-[350px] p-4 hover:bg-[#252528] rounded-lg group cursor-pointer flex flex-col ">
+        <div className="w-full h-[60%] rounded-full ">
+            <img className="w-full h-full object-contain rounded-full" src={item.artist.picture_big} alt="" />
+        </div>
+        <div className="text-primary font-serif text-xl pt-4">
+        <p>{item.artist.name}</p>
+        <p className="text-xs text-gray-600">Artist</p>
+      </div>
+    </div>
+  )
+}
+
+export default Artist
