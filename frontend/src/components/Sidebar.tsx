0ab18dbc1024f7f74c4c68d2e87653be26a30faa
Ayhan <ayhan-games@hotmail.com>
2025-03-16 04:33:23 +0300

    login signup added to siderbar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -4,64 +4,138 @@ import { Link } from "react-router-dom";
 import { IoRadioSharp } from "react-icons/io5";
 import { PiPlaylist } from "react-icons/pi";
 import { RiDvdFill } from "react-icons/ri";
-import { FaMusic } from "react-icons/fa";
+import { FaMusic, FaRupeeSign } from "react-icons/fa";
 import { MdOutlineVideoStable } from "react-icons/md";
+import { CiLogin } from "react-icons/ci";
 
 const Sidebar = () => {
-    const [activeMenu,setActiveMenu] = useState<string>("home");
+  const [activeMenu, setActiveMenu] = useState<string>("home");
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
   return (
-    <div className='w-[300px] h-full bg-[#212124] sticky top-0'>
-        <div className="w-full h-auto flex justify-between items-center p-7">
-            <div className="w-7 h-7 rounded-full bg-gray-600 flex items-center justify-center">
-                <p className="text-xs text-white ">GI</p>
-            </div>
-            <div className="text-white text-xl font-bold cursor-pointer">
-                <HiDotsHorizontal />
-            </div>
+    <div className="w-[300px] h-full bg-[#212124] sticky top-0">
+      <div className="w-full h-auto flex justify-between items-center p-7">
+        <div className="w-7 h-7 rounded-full bg-gray-600 flex items-center justify-center">
+          <p className="text-xs text-white ">GI</p>
         </div>
-        <div className="w-full h-auto flex p-7">
-            <div className="flex flex-col gap-8 text-base font-semibold ">
-                <Link onClick={()=>setActiveMenu("home")} className={`text-primary ${activeMenu ==="home" && "text-hover"}`} to="/">Home</Link>
-                <Link onClick={()=>setActiveMenu("explore")} className={`text-primary ${activeMenu ==="explore" && "text-hover"}`} to="/explore">Explore</Link>
-            </div>
+        <div
+          onClick={() => setOpenMenu(!openMenu)}
+          className="text-white text-xl font-bold cursor-pointer"
+        >
+          <HiDotsHorizontal />
         </div>
-        <div className="w-full h-auto">
-            <div className="p-4">
-                <p className="text-[#9898A6] text-sm">MY COLLECTIONS</p>
-            </div>
-            <div className="flex flex-col gap-8 text-base font-semibold px-7 ">
-                <div className="w-full flex gap-2 items-center ">
-                <IoRadioSharp  className="text-xl text-white" />
-                <Link onClick={()=>setActiveMenu("mixesandradio")} className={`text-primary ${activeMenu ==="mixesandradio" && "text-hover"}`} to="/">Mixes</Link>
-                
-                </div>
-                <div className="w-full flex gap-2 items-center ">
-                    <PiPlaylist className="text-xl text-white" />
-                <Link onClick={()=>setActiveMenu("playlists")} className={`text-primary ${activeMenu ==="playlists" && "text-hover"}`} to="/">Playlists</Link>
-                </div>
-                <div className="w-full flex gap-2 items-center ">
-                <RiDvdFill className="text-xl text-white" />
-                <Link onClick={()=>setActiveMenu("albums")} className={`text-primary ${activeMenu ==="albums" && "text-hover"}`} to="/albums">Albums And Artists</Link>
-                </div>
-                <div className="w-full flex gap-2 items-center ">
-                <FaMusic className="text-xl text-white"  />
-                <Link onClick={()=>setActiveMenu("tracks")} className={`text-primary ${activeMenu ==="tracks" && "text-hover"}`} to="/">Tracks</Link>
-                </div>
-                <div className="w-full flex gap-2 items-center ">
-                <MdOutlineVideoStable className="text-xl text-white" />
-                <Link onClick={()=>setActiveMenu("videos")} className={`text-primary ${activeMenu ==="videos" && "text-hover"}`} to="/">Videos</Link>
-                </div>
-            </div>
+      </div>
+      <div className="w-full h-auto flex p-7">
+        <div className="flex flex-col gap-8 text-base font-semibold ">
+          <Link
+            onClick={() => setActiveMenu("home")}
+            className={`text-primary ${activeMenu === "home" && "text-hover"}`}
+            to="/"
+          >
+            Home
+          </Link>
+          <Link
+            onClick={() => setActiveMenu("explore")}
+            className={`text-primary ${
+              activeMenu === "explore" && "text-hover"
+            }`}
+            to="/explore"
+          >
+            Explore
+          </Link>
         </div>
+      </div>
+      <div className="w-full h-auto">
+        <div className="p-4">
+          <p className="text-[#9898A6] text-sm">MY COLLECTIONS</p>
+        </div>
+        <div className="flex flex-col gap-8 text-base font-semibold px-7 ">
+          <div className="w-full flex gap-2 items-center ">
+            <IoRadioSharp className="text-xl text-white" />
+            <Link
+              onClick={() => setActiveMenu("mixesandradio")}
+              className={`text-primary ${
+                activeMenu === "mixesandradio" && "text-hover"
+              }`}
+              to="/"
+            >
+              Mixes
+            </Link>
+          </div>
+          <div className="w-full flex gap-2 items-center ">
+            <PiPlaylist className="text-xl text-white" />
+            <Link
+              onClick={() => setActiveMenu("playlists")}
+              className={`text-primary ${
+                activeMenu === "playlists" && "text-hover"
+              }`}
+              to="/"
+            >
+              Playlists
+            </Link>
+          </div>
+          <div className="w-full flex gap-2 items-center ">
+            <RiDvdFill className="text-xl text-white" />
+            <Link
+              onClick={() => setActiveMenu("albums")}
+              className={`text-primary ${
+                activeMenu === "albums" && "text-hover"
+              }`}
+              to="/albums"
+            >
+              Albums And Artists
+            </Link>
+          </div>
+          <div className="w-full flex gap-2 items-center ">
+            <FaMusic className="text-xl text-white" />
+            <Link
+              onClick={() => setActiveMenu("tracks")}
+              className={`text-primary ${
+                activeMenu === "tracks" && "text-hover"
+              }`}
+              to="/"
+            >
+              Tracks
+            </Link>
+          </div>
+          <div className="w-full flex gap-2 items-center ">
+            <MdOutlineVideoStable className="text-xl text-white" />
+            <Link
+              onClick={() => setActiveMenu("videos")}
+              className={`text-primary ${
+                activeMenu === "videos" && "text-hover"
+              }`}
+              to="/"
+            >
+              Videos
+            </Link>
+          </div>
+        </div>
+      </div>
 
-        <div className="w-full h-auto pt-5">
-            <div className="p-4">
-                <p className="text-[#9898A6] text-sm">MY PLAYLIST</p>
-            </div>
-            
+      <div className="w-full h-auto pt-5">
+        <div className="p-4">
+          <p className="text-[#9898A6] text-sm">MY PLAYLIST</p>
         </div>
+      </div>
+      {openMenu && <div className="w-full h-32 top-16  bg-primary absolute ">
+        <div className="w-full h-full flex items-center gap-5 pt-5 flex-col">
+            <div className="text-primary flex items-center gap-3">
+            <CiLogin className="text-3xl" />
+                <Link to={"/login"} className="text-primary font-bold">
+                 Login
+                </Link>
+            </div>
+            <div className="text-primary flex items-center gap-3 hover:text-hover">
+            <FaRupeeSign className="text-3xl"  /> 
+                <Link to={"/signup"} className="text-primary font-bold">
+                 Signup
+                </Link>
+            </div>
+
+        </div>
+        </div>}
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
