15ad42100e71159ea465b8ebae7d7641ca5533f2
Ayhan <ayhan-games@hotmail.com>
2025-03-19 01:38:03 +0300

    index.js updated

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,15 @@ const dotenv = require("dotenv");
 const authRouter = require("./router/UserRouter");
 dotenv.config();
 const app = express();
+const cookie = require("cookie-parser")
+const cors = require("cors");
 
+
+app.use(cookie());
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true
+}));
 app.use(express.json());
 app.use("/auth", authRouter);
 
