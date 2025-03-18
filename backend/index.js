44748fd2d67e6f23834455f19b3b7ecb21a87317
Ayhan <ayhan-games@hotmail.com>
2025-03-18 23:05:34 +0300

    index.js updated and necessary installation downloaded

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,12 @@
-const mysql = require("mysql2");
 const express = require("express");
 const dotenv = require("dotenv");
+const authRouter = require("./router/UserRouter");
 dotenv.config();
 const app = express();
 
+app.use(express.json());
+app.use("/auth", authRouter);
 
-
-const connectToDB = async()=>{
-    const db = mysql.createConnection({
-        host:"localhost",
-        user:"root",
-        password:process.env.db_password,
-        database:"music"
-    })
-    db.connect((err)=>{
-        if(err){
-            console.log("error while connecting to db",err);
-        }
-        else{
-            console.log("connected to DB successfully");
-            
-        }
-    })
-}
-
-app.listen((process.env.port),async()=>{
-    connectToDB();
-    console.log(`server listening on port ${process.env.port}`);
-    
-})
-
-
+app.listen(process.env.port, async () => {
+  console.log(`server listening on port ${process.env.port}`);
+});
