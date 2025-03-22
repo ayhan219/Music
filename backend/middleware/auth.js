cd5aade9b8e50f294017458b7623afa9abe66bf4
Ayhan <ayhan-games@hotmail.com>
2025-03-22 23:29:12 +0300

    auth middleware created for checking current user

---
+++ backend/middleware/auth.js
@@ -0,0 +1,35 @@
+const jwt = require("jsonwebtoken");
+const db = require("../db");
+
+
+const verifyToken = (req, res, next) => {
+
+    const token = req.headers.cookie?.split("=")[1]
+    if(!token){
+        return res.status(401).json({message:"unauthorized"})
+    }
+    try {
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+        const sql = "select * from user where id=?";
+        db.query(sql,[decoded.id],(err,result)=>{
+            if(err){
+                return res.status(500).json({message:"server error"})
+            }
+            if(result.length===0){
+                return res.status(401).json({message:"unauthorized"})
+            }
+            const {password,...rest}= result[0]
+            
+            req.user = rest
+            next();
+            
+        })
+        
+    } catch (error) {
+        return res.status(500).json({message:"server error"})
+    }
+    
+
+}
+
+module.exports = verifyToken;
