9016327cfc39977f2322f50fef7dfec9f761a3e1
Ayhan <ayhan-games@hotmail.com>
2025-03-22 23:30:23 +0300

    getuser and logout functionality added

--- backend/router/UserRouter.js
+++ backend/router/UserRouter.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {signup,login} = require("../controller/UserController")
+const AuthMiddleware = require("../middleware/auth")
+const {signup,login,getUser,logout} = require("../controller/UserController")
 
 
 router.post("/signup",signup);
 router.post("/login",login)
+router.get("/getuser",AuthMiddleware,getUser)
+router.delete("/logout",logout)
 
 module.exports = router;
