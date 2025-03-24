7635e0ab21b7c1d5ab8c73aaa7adfc67f3bb4c14
Ayhan <ayhan-games@hotmail.com>
2025-03-24 16:23:07 +0300

    google auth 2.0 functionality added

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -1,10 +1,10 @@
 const db = require("../db");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const passport = require("passport");
 
 const signup = async (req, res) => {
   const { username, email, password } = req.body;
-  console.log(username, email, password);
 
   if (!username || !email || !password) {
     return res.status(400).json({ message: "provide all area" });
@@ -73,14 +73,84 @@ const getUser = async (req, res) => {
   res.status(200).json(req.user);
 };
 
-const logout = async(req,res)=>{
+const logout = async (req, res) => {
   res.clearCookie("token");
-  res.status(200).json({message:"logout successfull"});
-}
+  res.status(200).json({ message: "logout successfull" });
+};
+
+const authGoogle = async (req, res, next) => {
+
+  passport.authenticate("google", { scope: ["profile", "email"] })(
+    req,
+    res,
+    next
+  );
+};
+
+const googleCallback = async (req, res) => {
+  passport.authenticate("google", async (err, user) => {
+    if (err) {
+      return res.status(500).json({ message: "Server error", err });
+    }
+
+    const userData = {
+      username: user.displayName.replace(/\s+/g, "_"),
+      email: user.emails[0].value,
+    };
+
+    const sql = "SELECT * FROM user WHERE email=?";
+    try {
+      db.query(sql, [userData.email], (err, results) => {
+        if (err) {
+          return res.status(500).json({ message: "Database error", err });
+        }
+
+        if (results.length === 0) {
+          const insertSql = "INSERT INTO user (username, email) VALUES (?, ?)";
+          db.query(
+            insertSql,
+            [userData.username, userData.email],
+            (err, result) => {
+              if (err) {
+                return res
+                  .status(500)
+                  .json({ message: "Error inserting user", err });
+              }
+              res.redirect("http://localhost:5173/redirect");
+            }
+          );
+        } else {
+          const token = jwt.sign(
+            {
+              id: results[0].id,
+              username: results[0].username.replace(/\s+/g, "_"),
+            },
+            process.env.JWT_SECRET,
+            {
+              expiresIn: "1h",
+            }
+          );
+          res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV === 'production',
+            maxAge: 3600000,
+          });
+          res.redirect("http://localhost:5173/redirect");
+        }
+      });
+    } catch (error) {
+      return res.status(500).json({ message: "Server error", error });
+    }
+  })(req, res);
+};
 
 module.exports = {
   signup,
   login,
   getUser,
-  logout
+  logout,
+  authGoogle,
+  googleCallback,
 };
