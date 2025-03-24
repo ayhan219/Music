2992d6912b527837d9d4d96b9e75de9d59d00b02
Ayhan <ayhan-games@hotmail.com>
2025-03-24 16:24:18 +0300

    google auth router added

--- backend/router/UserRouter.js
+++ backend/router/UserRouter.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const AuthMiddleware = require("../middleware/auth")
-const {signup,login,getUser,logout} = require("../controller/UserController")
+const {signup,login,getUser,logout,authGoogle,googleCallback} = require("../controller/UserController")
 
 
 router.post("/signup",signup);
 router.post("/login",login)
 router.get("/getuser",AuthMiddleware,getUser)
 router.delete("/logout",logout)
+router.get('/google', authGoogle);
+router.get("/google/callback",googleCallback)
 
 module.exports = router;
