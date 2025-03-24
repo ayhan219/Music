c9de280f19d260262145f36134487cc95b272de0
Ayhan <ayhan-games@hotmail.com>
2025-03-24 16:23:50 +0300

    taking correct token

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,12 +4,18 @@ const db = require("../db");
 
 const verifyToken = (req, res, next) => {
 
-    const token = req.headers.cookie?.split("=")[1]
+    const token = req.headers.cookie
     if(!token){
         return res.status(401).json({message:"unauthorized"})
     }
+    const tokenValue = token.split(';').find(c => c.trim().startsWith('token='))
+    if (!tokenValue) {
+        return res.status(401).json({ message: "unauthorized - token not found" });
+    }
+    const jwtToken = tokenValue.split('=')[1];
+    
     try {
-        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+        const decoded = jwt.verify(jwtToken,process.env.JWT_SECRET);
         const sql = "select * from user where id=?";
         db.query(sql,[decoded.id],(err,result)=>{
             if(err){
@@ -26,7 +32,7 @@ const verifyToken = (req, res, next) => {
         })
         
     } catch (error) {
-        return res.status(500).json({message:"server error"})
+        return res.status(500).json({message:"server error",error})
     }
     
 
