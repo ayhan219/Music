8f929b3a036c92ebfb59ceff0ca74a41b602cbab
Ayhan <ayhan-games@hotmail.com>
2025-03-26 03:49:06 +0300

    bug fixed

--- backend/controller/MusicController.js
+++ backend/controller/MusicController.js
@@ -1,24 +1,41 @@
 const db = require("../db");
 
+const createPlaylist = async (req, res) => {
+  const { user_id, playlist_name, playlist_description } = req.body;
+  const query =
+    "INSERT INTO playlists (user_id, playlist_name, playlist_description) VALUES (?, ?, ?)";
 
-const createPlaylist = async(req,res)=>{
-    const {user_id,playlist_name,playlist_description} = req.body;
-    const query = "insert into playlists (user_id,playlist_name,playlist_description) value(?,?,?)";
-    try {
-        db.query(query,[user_id,playlist_name,playlist_description],(err,result)=>{
-            if(err){
-                res.status(500).json({message:"error occured while creating playlist"})
-            }
-            else{
-                res.status(200).json({message:"playlist created successfully"})
-            }
-        })
-    } catch (error) {
-        return res.status(500).json({message:"error occured while creating playlist"})
-    }
-}
+  try {
+    db.query(
+      query,
+      [user_id, playlist_name, playlist_description],
+      (err, result) => {
+        if (err) {
+          return res
+            .status(500)
+            .json({ message: "Error occurred while creating playlist" });
+        }
+        const lastInsertedId = result.insertId;
+        const query2 = "SELECT * FROM playlists WHERE id = ?";
 
+        db.query(query2, [lastInsertedId], (err, playlistResult) => {
+          if (err) {
+            return res
+              .status(500)
+              .json({ message: "Error occurred while fetching playlist" });
+          }
+
+          res.status(200).json(playlistResult[0]);
+        });
+      }
+    );
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error occurred while creating playlist" });
+  }
+};
 
 module.exports = {
-    createPlaylist
-}
+  createPlaylist,
+};
