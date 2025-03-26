43cdfbbe29c637ffaf2221e41ef89194e4e62a17
Ayhan <ayhan-games@hotmail.com>
2025-03-26 03:51:31 +0300

    createplaylist handled

--- frontend/src/components/CreatePlaylist.tsx
+++ frontend/src/components/CreatePlaylist.tsx
@@ -1,10 +1,32 @@
 import { IoMdCloseCircle } from "react-icons/io";
+import axios from "axios"
+import { useSelector } from "react-redux";
+import { RootState } from "../app/store";
+import { useState } from "react";
 
 interface CreatePlaylistProps {
   setOpenCreatePlaylist: (value: boolean) => void;
 }
 
 const CreatePlaylist = ({ setOpenCreatePlaylist }: CreatePlaylistProps) => {
+  const user = useSelector((state:RootState)=>state.userSlice.user);
+  const [playlistName,setPlaylistName] = useState<string>("");
+  const [playlistDescription,setPlaylistDescription] = useState<string>("");
+
+  const handleCreatePlaylist = async()=>{
+    try {
+      const response = await axios.post("http://localhost:5000/music/createplaylist",{
+        user_id:user.id,
+        playlist_name:playlistName,
+        playlist_description:playlistDescription
+      })
+      console.log(response.data);
+      
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
   return (
     <div className="inset-0 fixed z-[1000] bg-black/80 flex items-center justify-center">
       <div className="w-[450px] h-[500px] bg-[#1F1F22] rounded-xl p-6 flex flex-col justify-evenly">
@@ -19,6 +41,7 @@ const CreatePlaylist = ({ setOpenCreatePlaylist }: CreatePlaylistProps) => {
         <div className="w-full mb-4">
           <label className="text-white text-sm mb-2 block">Playlist Name</label>
           <input
+          onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setPlaylistName(e.target.value)}
             type="text"
             placeholder="Enter playlist name"
             className="w-full p-3 bg-[#282828] text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-[#1DB954]"
@@ -28,6 +51,7 @@ const CreatePlaylist = ({ setOpenCreatePlaylist }: CreatePlaylistProps) => {
         <div className="w-full mb-4">
           <label className="text-white text-sm mb-2 block">Description</label>
           <textarea
+          onChange={(e:React.ChangeEvent<HTMLTextAreaElement>)=>setPlaylistDescription(e.target.value)}
             placeholder="Enter playlist description (optional)"
             className="w-full p-3 bg-[#282828] text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-[#1DB954]"
           />
@@ -42,6 +66,7 @@ const CreatePlaylist = ({ setOpenCreatePlaylist }: CreatePlaylistProps) => {
             Cancel
           </button>
           <button
+          onClick={handleCreatePlaylist}
             className="w-1/2 p-3 cursor-pointer bg-[#1DB954] text-white rounded-lg hover:bg-[#1AA34A]"
           >
             Create Playlist
