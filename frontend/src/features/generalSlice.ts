f66aa011ef339976c56a0de9bab8d5f7edeaf592
Ayhan <ayhan-games@hotmail.com>
2025-03-31 02:57:41 +0300

    contextmenu added to global

--- frontend/src/features/generalSlice.ts
+++ frontend/src/features/generalSlice.ts
@@ -1,26 +1,35 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-
-interface GeneralType{
-    input:string
+interface GeneralType {
+  input: string;
+  contextMenu: { visible: boolean; x: number; y: number; id: number | null }; 
 }
 
-const initialState : GeneralType = {
-    input:""  
-}
-
+const initialState: GeneralType = {
+  input: "",
+  contextMenu: { visible: false, x: 0, y: 0, id: null }, 
+};
 
 export const generalSlice = createSlice({
-    name:"generalSlice",
-    initialState,
-    reducers:{
-        setInput:(state,action:PayloadAction<string>)=>{
-            state.input = action.payload;
-        }
-    }
-    
+  name: "generalSlice",
+  initialState,
+  reducers: {
+    setInput: (state, action: PayloadAction<string>) => {
+      state.input = action.payload;
+    },
+    setContextMenu: (
+      state,
+      action: PayloadAction<{ visible: boolean; x: number; y: number; id: number | null }>
+    ) => {
+      state.contextMenu = {
+        visible: action.payload.visible,
+        x: action.payload.x,
+        y: action.payload.y,
+        id: action.payload.id, 
+      };
+    },
+  },
+});
 
-})
-
-export const {setInput} = generalSlice.actions;
+export const { setInput, setContextMenu } = generalSlice.actions;
 export default generalSlice.reducer;
