108414d939d724f68e3effe1d4200427ff33dcf0
Ayhan <ayhan-games@hotmail.com>
2025-03-31 02:55:42 +0300

    contextmenu added to every single music and handling addplaylist

--- frontend/src/components/Music.tsx
+++ frontend/src/components/Music.tsx
@@ -7,6 +7,9 @@ import {
 } from "../features/PlayingMusicSlice";
 import { RootState } from "../app/store";
 import { setCurrentAlbumMusic } from "../features/MusicSlice";
+import { useRef, useState } from "react";
+import { setContextMenu } from "../features/generalSlice";
+import axios from "axios";
 
 interface AlbumMusic {
   artist: {
@@ -24,79 +27,169 @@ interface AlbumMusic {
 }
 
 interface MusicProps {
-  item: {
-    artist: {
-      name: string;
-    };
-    album: {
-      cover_medium: string;
-    };
-    duration: number;
-    id: number;
-    md5_image: string;
-    preview: string;
-    rank: number;
-    title: string;
-  };
+  item: AlbumMusic;
   index: number;
   whichMusic: AlbumMusic[] | undefined;
 }
 
-const Music = ({ item, index, whichMusic }: MusicProps) => {
+const Music = ({ item, index, whichMusic }: MusicProps | any) => {
   const musicId = useSelector((state: RootState) => state.musicPlayer.musicId);
+  const user = useSelector((state: RootState) => state.userSlice.user);
+  const [hoveredItem, setHoveredItem] = useState<number | null>(null);
+  const contextMenu = useSelector(
+    (state: RootState) => state.generalData.contextMenu
+  );
+
+  const musicRef = useRef<HTMLDivElement>(null);
 
   const dispatch = useDispatch();
+
+  const handleContextMenu = (e: React.MouseEvent) => {
+    e.preventDefault();
+
+    if (musicRef.current) {
+      const rect = musicRef.current.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+
+      dispatch(
+        setContextMenu({
+          visible: true,
+          x,
+          y,
+          id: item.id,
+        })
+      );
+    }
+  };
+
+  const handleClickOutside = () => {
+    dispatch(
+      setContextMenu({
+        visible: false,
+        x: 0,
+        y: 0,
+        id: null,
+      })
+    );
+  };
+
+  const handleAddToPlaylist = async (playlist_id: number) => {
+    const musicData = {
+      user_id: user.id,
+      playlist_id: playlist_id,
+      music_name: item.title,
+      artist: item.artist.name,
+      music_url: item.preview,
+      music_image: item.md5_image,
+      music_duration: item.duration,
+      music_rank: item.rank,
+    };
+    try {
+      await axios.post("http://localhost:5000/music/addmusictoplaylist", {
+        musicData,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div
-      key={index}
-      onClick={() => {
-        dispatch(setMusicId(item.id));
-        dispatch(setCurrentAlbumMusic(whichMusic || []));
-        dispatch(setOpenMusicBar(true));
-        dispatch(setIsPlaying(true));
-      }}
-      className={`w-full  h-16 flex items-center py-4 px-4  group cursor-pointer hover:bg-[#262629]   group`}
+      onMouseLeave={() =>
+        dispatch(
+          setContextMenu({
+            visible: false,
+            x: 0,
+            y: 0,
+            id: null,
+          })
+        )
+      }
+      ref={musicRef}
+      className="relative"
+      onClick={handleClickOutside}
     >
-      <div className="flex items-center gap-4 w-[50%]  ">
-        <div className="w-20 h-20 relative flex   items-center justify-center  cursor-pointer">
-          <img
-            className="w-16 h-16 object-cover rounded-lg"
-            src={item.album.cover_medium}
-            alt="Album Cover"
-          />
-          <FaPlay className="absolute text-primary text-xl opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+      <div
+        key={index}
+        onContextMenu={handleContextMenu}
+        onClick={() => {
+          dispatch(setMusicId(item.id));
+          dispatch(setCurrentAlbumMusic(whichMusic || []));
+          dispatch(setOpenMusicBar(true));
+          dispatch(setIsPlaying(true));
+        }}
+        className="w-full h-16 flex items-center py-4 px-4 group cursor-pointer hover:bg-[#262629]"
+      >
+        <div className="flex items-center gap-4 w-[50%]">
+          <div className="w-20 h-20 relative flex items-center justify-center cursor-pointer">
+            <img
+              className="w-16 h-16 object-cover rounded-lg"
+              src={`https://cdn-images.dzcdn.net/images/cover/${item.md5_image}/500x500-000000-80-0-0.jpg`}
+              alt="Album Cover"
+            />
+            <FaPlay className="absolute text-primary text-xl opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+          </div>
+          <div className="flex flex-col gap-1 w-[500px] truncate">
+            <p
+              className={`${
+                item.id === musicId
+                  ? "text-green-500 font-extrabold"
+                  : "text-primary font-semibold"
+              } truncate`}
+            >
+              {item.title}
+            </p>
+            <p
+              className={`${
+                item.id === musicId ? "text-white font-bold" : "text-[#595956]"
+              } text-sm truncate`}
+            >
+              {item.artist.name}
+            </p>
+          </div>
         </div>
-        <div className="flex flex-col gap-1 w-[500px] truncate">
-          <p
-            className={`${
-              item.id === musicId
-                ? "text-green-500 font-extrabold"
-                : "text-primary font-semibold"
-            }   truncate`}
-          >
-            {item.title}
-          </p>
-          <p
-            className={`ease-in-out ${
-              item.id === musicId ? "text-white font-bold" : "text-[#595956]"
-            } duration-100  text-sm truncate`}
-          >
-            {item.artist.name}
-          </p>
-          
-        </div>
-        
-          
-      </div>
-      <div className="w-full flex justify-between px-32  text-primary ">
-          <div className="">
+
+        <div className="w-full flex justify-between px-32 text-primary">
+          <div>
             <p>{item.rank}</p>
           </div>
           <p>
             {Math.floor(item.duration / 60)}:
             {(item.duration % 60).toString().padStart(2, "0")}
           </p>
-          </div>
+        </div>
+      </div>
+
+      {contextMenu.id === item.id && contextMenu.visible && (
+        <ul
+          className="absolute z-50 bg-gray-800 text-white shadow-lg border border-gray-600 rounded-md w-40 text-sm"
+          style={{ top: `${contextMenu.y}px`, left: `${contextMenu.x}px` }}
+        >
+          <li
+            className="p-2 hover:bg-gray-700 cursor-pointer"
+            onMouseEnter={() => setHoveredItem(item.id)}
+          >
+            Add to Playlist
+            {hoveredItem === item.id && (
+              <ul
+                onMouseLeave={() => setHoveredItem(null)}
+                className="absolute left-40 top-0 bg-gray-700 text-white w-40 rounded-md"
+              >
+                {user.playlists?.map((item, index) => (
+                  <li
+                    onClick={() => handleAddToPlaylist(item.playlist_id)}
+                    key={index}
+                    className="p-2 cursor-pointer hover:bg-gray-600"
+                  >
+                    {item.playlist_name}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
