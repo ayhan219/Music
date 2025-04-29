5bd4d59b8e30315602f5316df9b006c6723bcfd2
Ayhan <ayhan-games@hotmail.com>
2025-04-30 00:36:43 +0300

    added released albums and scrollbar made

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,25 +6,27 @@ import {
   MdOutlineKeyboardArrowRight,
 } from "react-icons/md";
 import { motion } from "framer-motion";
-import { getPopularAlbums } from "../features/MusicSlice";
-import { useAppDispatch } from "../app/hooks";
+import { getPopularAlbums, getReleasedAlbums } from "../features/MusicSlice";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
 import { useSelector } from "react-redux";
 import { RootState } from "../app/store";
 
-
 const Home = () => {
-  const albums = useSelector((state:RootState)=>state.albumMusic.popularAlbums)
+  const albums = useSelector(
+    (state: RootState) => state.albumMusic.popularAlbums
+  );
+  const releasedAlbums = useAppSelector((state:RootState)=>state.albumMusic.releasedAlbums);
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const [currentIndex2, setCurrentIndex2] = useState<number>(0);
-  const loading = useSelector((state:RootState)=>state.albumMusic.loading)
-  const user = useSelector((state:RootState)=>state.userSlice.user)
+  const [currentIndex3, setCurrentIndex3] = useState<number>(0);
+  const loading = useSelector((state: RootState) => state.albumMusic.loading);
+  const user = useSelector((state: RootState) => state.userSlice.user);
 
   const dispatch = useAppDispatch();
 
-
   useEffect(() => {
-    dispatch(getPopularAlbums())
-    
+    dispatch(getPopularAlbums());
+    dispatch(getReleasedAlbums())
   }, [dispatch]);
 
   const handleNext = () => {
@@ -39,7 +41,6 @@ const Home = () => {
     }
   };
 
-
   const handleNext2 = () => {
     if (currentIndex2 + 6 < albums?.length) {
       setCurrentIndex2(currentIndex2 + 6);
@@ -52,8 +53,20 @@ const Home = () => {
     }
   };
 
+  const handleNext3 = () => {
+    if (currentIndex3 + 6 < releasedAlbums?.length) {
+      setCurrentIndex3(currentIndex3 + 6);
+    }
+  };
+
+  const handlePrev3 = () => {
+    if (currentIndex3 - 6 >= 0) {
+      setCurrentIndex3(currentIndex3 - 6);
+    }
+  };
+
   return (
-    <div className="w-full  h-[90%] overflow-y-hidden bg-primary">
+    <div className="w-full  h-[90%] overflow-y-auto scrollbar-custom bg-primary">
       <div className="px-24 w-full flex flex-col  gap-8">
         {!loading ? (
           <>
@@ -93,39 +106,77 @@ const Home = () => {
 
             <div className="pt-3">
               <div className="w-full flex justify-between">
-              <div className="text-primary text-xl">
-                <h3>My List</h3>
-              </div>
-              <div className="flex gap-2">
-                  <div
-                    className="w-7 h-7 rounded-full bg-[#404048] flex items-center justify-center cursor-pointer"
-                  onClick={handlePrev2}
-                  >
-                    <MdOutlineKeyboardArrowLeft className="text-white text-2xl" />
+                <div className="text-primary text-xl">
+                  <h3>Released Albums</h3>
+                </div>
+                <div className="flex gap-2">
+                  <div className="w-7 h-7 rounded-full bg-[#404048] flex items-center justify-center cursor-pointer">
+                    <MdOutlineKeyboardArrowLeft className="text-white text-2xl"
+                     onClick={handlePrev3}
+                    />
+                   
                   </div>
-                  <div
-                    className="w-7 h-7 rounded-full bg-[#404048] flex items-center justify-center cursor-pointer"
-                  onClick={handleNext2}
-                  >
-                    <MdOutlineKeyboardArrowRight className="text-white text-2xl" />
+                  <div className="w-7 h-7 rounded-full bg-[#404048] flex items-center justify-center cursor-pointer">
+                    <MdOutlineKeyboardArrowRight className="text-white text-2xl"
+                     onClick={handleNext3}
+                    />
                   </div>
                 </div>
               </div>
               <motion.div
-                className="flex flex-row gap-3 pt-3 overflow-x-auto"
-                key={currentIndex2}
+                className="flex flex-row gap-2 pt-3 overflow-x-auto"
+                key={currentIndex3}
                 initial={{ opacity: 0, scale: 0.8 }}
                 animate={{ opacity: 1, scale: 1 }}
                 exit={{ opacity: 0, scale: 0.8 }}
                 transition={{ duration: 0.5, ease: "easeInOut" }}
               >
-                {
-                  user?.playlists?.slice(currentIndex2, currentIndex2 + 6).map((item,key)=>(
-                    <Album2 key={key} item={item} index={key} />
-                  ))
-                }
+                {releasedAlbums.slice(0,6).map((item) => (
+                  <Album key={item.id} item={item} />
+                ))}
               </motion.div>
+
             </div>
+
+            {user.id && (
+              <>
+                <div className="pt-3">
+                  <div className="w-full flex justify-between">
+                    <div className="text-primary text-xl">
+                      <h3>My List</h3>
+                    </div>
+                    <div className="flex gap-2">
+                      <div
+                        className="w-7 h-7 rounded-full bg-[#404048] flex items-center justify-center cursor-pointer"
+                        onClick={handlePrev2}
+                      >
+                        <MdOutlineKeyboardArrowLeft className="text-white text-2xl" />
+                      </div>
+                      <div
+                        className="w-7 h-7 rounded-full bg-[#404048] flex items-center justify-center cursor-pointer"
+                        onClick={handleNext2}
+                      >
+                        <MdOutlineKeyboardArrowRight className="text-white text-2xl" />
+                      </div>
+                    </div>
+                  </div>
+                  <motion.div
+                    className="flex flex-row gap-3 pt-3 overflow-x-auto"
+                    key={currentIndex2}
+                    initial={{ opacity: 0, scale: 0.8 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    exit={{ opacity: 0, scale: 0.8 }}
+                    transition={{ duration: 0.5, ease: "easeInOut" }}
+                  >
+                    {user?.playlists
+                      ?.slice(currentIndex2, currentIndex2 + 6)
+                      .map((item, key) => (
+                        <Album2 key={key} item={item} index={key} />
+                      ))}
+                  </motion.div>
+                </div>
+              </>
+            )}
           </>
         ) : (
           <div className="flex justify-center items-center space-x-2">
