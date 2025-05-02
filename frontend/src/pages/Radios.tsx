77261e25e1038809b9e29af361dfba6036ca3ff6
Ayhan <ayhan-games@hotmail.com>
2025-05-03 00:37:07 +0300

    displaying all radios

---
+++ frontend/src/pages/Radios.tsx
@@ -0,0 +1,25 @@
+import { useAppSelector } from "../app/hooks"
+import { RootState } from "../app/store"
+import RadioComp from "../components/RadioComp";
+
+
+const Radios = () => {
+    const radios = useAppSelector((state:RootState)=>state.albumMusic.radio);
+
+  return (
+    <div className="w-full h-[90%] bg-primary overflow-y-auto scrollbar-custom px-16 ">
+      <div className="text-white text-3xl font-bold mb-3 px-5">
+        <h1>Radios</h1>
+      </div>
+      <div className="w-full grid grid-cols-5 gap-10">
+        {
+            radios.map((item,index)=>(
+                <RadioComp item={item} index={index} />
+            ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Radios
