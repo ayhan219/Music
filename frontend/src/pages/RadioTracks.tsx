627f4c696bf2c584596c94715578717c89ba3e27
Ayhan <ayhan-games@hotmail.com>
2025-05-04 13:38:55 +0300

    displaying all radios on this page

---
+++ frontend/src/pages/RadioTracks.tsx
@@ -0,0 +1,28 @@
+import { useAppSelector } from "../app/hooks";
+import { RootState } from "../app/store";
+import RadioSingle from "../components/RadioSingle";
+
+const RadioTracks = () => {
+  const { radios, loading } = useAppSelector(
+    (state: RootState) => state.albumMusic
+  );
+  return (
+    <div className="w-full h-[90%] bg-primary p-8 gap-8">
+      <div className="w-full h-full grid grid-cols-3 gap-10 overflow-y-auto scrollbar-custom">
+        {loading ? (
+          <div className="absolute top-1/2 left-[60%] -translate-x-1/2 -translate-y-1/2">
+            <div className="w-12 h-12 border-4 border-t-4 border-t-blue-500 border-gray-300 rounded-full animate-spin"></div>
+          </div>
+        ) : (
+          <>
+            {radios.map((item, index) => (
+              <RadioSingle item={item} index={index} />
+            ))}
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default RadioTracks;
