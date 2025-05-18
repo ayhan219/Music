3a1a76c20b6c281c3bed2332e6fe0aae741861ff
Ayhan <ayhan-games@hotmail.com>
2025-05-19 01:01:10 +0300

    responsive design added

--- frontend/src/pages/Playlists.tsx
+++ frontend/src/pages/Playlists.tsx
@@ -10,10 +10,10 @@ const Playlists = () => {
   return (
     <div className="w-full min-h-[90%] bg-primary p-8 sm:p-10 md:p-14">
       <div className="w-full text-white font-bold font-mono text-3xl border-b border-gray-600 pb-4 mb-6">
-        <h1 onClick={() => console.log(user)}>🎵 My Playlists</h1>
+        <h1>🎵 My Playlists</h1>
       </div>
 
-      <div className="h-[670px] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-6 overflow-y-auto overflow-x-hidden scrollbar-custom">
+      <div className="h-[670px] grid place-items-center md:place-items-baseline grid-cols-1  sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-6 overflow-y-auto overflow-x-hidden scrollbar-custom">
         {loading ? (
           <div className="flex justify-center items-center h-full">
             <div className="w-12 h-12 border-4 border-t-4 border-t-blue-500 border-gray-300 rounded-full animate-spin"></div>
