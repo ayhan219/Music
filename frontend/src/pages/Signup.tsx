0a53d20c19399723e37b1a3fb1acfe2226e5ce06
Ayhan <ayhan-games@hotmail.com>
2025-05-21 00:04:13 +0300

    added responsive style

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -56,32 +56,32 @@ const Signup = () => {
 
   return (
     <div className="w-full h-screen bg-primary flex items-center justify-center">
-      <div className="w-[1000px] h-[800px] flex rounded-xl shadow-xl">
-        <div className="w-[400px] h-full rounded-l-xl overflow-hidden">
+      <div className="w-[300px] sm:w-[400px] md:w-[1000px] h-[800px] flex flex-col items-center md:flex-row rounded-xl shadow-xl">
+        <div className="w-full md:w-[400px] rounded-l-xl overflow-hidden">
           <img
             className="w-full h-full object-cover"
             src={mImage}
             alt="Background"
           />
         </div>
-        <div className="w-[600px] h-full bg-[#F7F7F7] rounded-r-xl p-10 flex flex-col justify-between">
+        <div className="w-[300px] sm:w-[400px] md:w-[600px] h-[90%] md:h-full bg-[#F7F7F7] rounded-r-xl p-10 flex flex-col justify-between">
           <div className="w-full text-center">
-            <h1 className="text-5xl font-bold text-gray-800 mb-5">Sign Up</h1>
-            <p className="text-lg text-gray-600">
+            <h1 className="text-2xl md:text-5xl font-bold text-gray-800 mb-5">Sign Up</h1>
+            <p className="text-base md:text-lg text-gray-600">
               Create a new account to get started.
             </p>
           </div>
 
           <div className="w-full mt-6 mb-6">
-            <div className="w-full h-14 bg-white rounded-lg flex items-center justify-center shadow-lg hover:shadow-2xl transition-all duration-300 cursor-pointer">
+            <div className="w-full h-10 md:h-14 bg-white rounded-lg flex items-center justify-center shadow-lg hover:shadow-2xl transition-all duration-300 cursor-pointer">
               <FcGoogle className="text-2xl mr-3" />
-              <p className="text-lg font-semibold text-gray-700">
+              <p className="text-sm md:text-lg font-semibold text-gray-700">
                 Sign up with Google
               </p>
             </div>
           </div>
 
-          <div className="w-full flex flex-col space-y-6">
+          <div className="w-full flex flex-col space-y-3 md:space-y-6">
             <div>
               <label className="block text-gray-700 text-sm font-semibold mb-2">
                 Username
@@ -90,7 +90,7 @@ const Signup = () => {
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                   setUsername(e.target.value)
                 }
-                className="w-full h-12 px-4 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full h-8 md:h-12 px-1 md:px-4 placeholder:text-sm rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 type="text"
                 placeholder="Enter your username"
               />
@@ -103,7 +103,7 @@ const Signup = () => {
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                   setEmail(e.target.value)
                 }
-                className="w-full h-12 px-4 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full h-8 md:h-12 px-1 md:px-4 rounded-lg placeholder:text-sm border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 type="email"
                 placeholder="Enter your email"
               />
@@ -116,7 +116,7 @@ const Signup = () => {
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                   setPassword(e.target.value)
                 }
-                className="w-full h-12 px-4 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full h-8 md:h-12  px-1 md:px-4 placeholder:text-sm rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 type="password"
                 placeholder="Enter your password"
               />
@@ -129,7 +129,7 @@ const Signup = () => {
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                   setRePassword(e.target.value)
                 }
-                className="w-full h-12 px-4 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full h-8 md:h-12 px-1 md:px-4 placeholder:text-sm rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 type="password"
                 placeholder="Confirm your password"
               />
@@ -140,7 +140,7 @@ const Signup = () => {
           <button
               disabled={loading}
               onClick={handleSignup}
-              className={`w-full h-12 text-center flex justify-center items-center bg-blue-500 text-white rounded-lg font-semibold hover:bg-blue-600 transition-colors duration-300 ${
+              className={`w-full h-10 md:h-12 text-center flex justify-center items-center bg-blue-500 text-white rounded-lg font-semibold hover:bg-blue-600 transition-colors duration-300 ${
                 loading
                   ? "bg-[#1AA34A] cursor-not-allowed opacity-75"
                   : "bg-[#1DB954] hover:bg-[#1AA34A]"
