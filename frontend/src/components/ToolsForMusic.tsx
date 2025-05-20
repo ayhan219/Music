ded5952245bfcbb33c633d2168091184e3c5ea96
Ayhan <ayhan-games@hotmail.com>
2025-05-20 16:05:35 +0300

    responsive design added

--- frontend/src/components/ToolsForMusic.tsx
+++ frontend/src/components/ToolsForMusic.tsx
@@ -28,21 +28,21 @@ const ToolsForMusic = () => {
     }
   };
   return (
-    <div className="w-full h-32 px-6 flex items-center text-primary">
+    <div className="w-full h-32 px-4 md:px-6 flex items-center text-primary">
       <div className="w-full flex items-center gap-4">
         <div
           onClick={() => handleClickMusicButton()}
-          className="w-12 h-12 rounded-full flex items-center justify-center bg-green-500 cursor-pointer"
+          className="w-8 h-8 md:w-12 md:h-12 rounded-full flex items-center justify-center bg-green-500 cursor-pointer"
         >
-          <FaPlay className="text-black" />
+          <FaPlay className="text-black text-sm md:text-base" />
         </div>
-        <div className="w-12 h-12 text-primary text-3xl flex items-center justify-center ">
+        <div className="w-8 h-8 md:w-12 text-primary text-xl md:text-3xl flex items-center justify-center ">
           <VscArrowSwap />
         </div>
-        <div className="w-20 h-8 rounded-lg border flex items-center justify-center">
+        <div className="w-10 md:w-20 h-8 rounded-lg border flex text-xs md:text-base items-center justify-center">
           Follow
         </div>
-        <div className="w-12 h-12 text-primary text-3xl flex items-center justify-center">
+        <div className="w-5 h-8 md:w-12 text-primary text-3xl flex items-center justify-center">
           <BsThreeDots />
         </div>
       </div>
