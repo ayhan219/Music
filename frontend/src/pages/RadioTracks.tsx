a2c7e12a7920cdba51e3a06054bdd15fc7446b99
Ayhan <ayhan-games@hotmail.com>
2025-05-22 00:04:53 +0300

    grid added and responsive design fixed

--- frontend/src/pages/RadioTracks.tsx
+++ frontend/src/pages/RadioTracks.tsx
@@ -7,8 +7,8 @@ const RadioTracks = () => {
     (state: RootState) => state.albumMusic
   );
   return (
-    <div className="w-full h-[90%] bg-primary p-8 gap-8">
-      <div className="w-full h-full grid grid-cols-4 gap-10 overflow-y-auto scrollbar-custom">
+    <div className="w-full h-[90%] bg-primary p-3 md:p-8 gap-8">
+      <div className="w-full h-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 place-items-center  xl:grid-cols-5 overflow-x-hidden gap-8 overflow-y-auto scrollbar-custom pb-24">
         {loading ? (
           <div className="absolute top-1/2 left-[60%] -translate-x-1/2 -translate-y-1/2">
             <div className="w-12 h-12 border-4 border-t-4 border-t-blue-500 border-gray-300 rounded-full animate-spin"></div>
