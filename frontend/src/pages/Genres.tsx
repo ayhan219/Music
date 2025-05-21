c49386ed1618c91ed35a52e651fa4d449fc3a856
Ayhan <ayhan-games@hotmail.com>
2025-05-22 00:02:50 +0300

    grid added

--- frontend/src/pages/Genres.tsx
+++ frontend/src/pages/Genres.tsx
@@ -7,11 +7,11 @@ const Genres = () => {
     const genres = useAppSelector((state:RootState)=>state.albumMusic.genres);
 
   return (
-    <div className="w-full h-full bg-primary overflow-y-auto scrollbar-custom px-16 pb-20 ">
+    <div className="w-full h-full bg-primary overflow-y-auto scrollbar-custom px-16 pb-36 ">
       <div className="text-white text-3xl font-bold mb-3 px-5">
         <h1>Genres</h1>
       </div>
-      <div className="w-full grid grid-cols-5 gap-10">
+      <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 place-items-center  xl:grid-cols-5 gap-6">
         {
             genres.map((item,index)=>(
                 <ArtistComp item={item} index={index} />
