0b0c2958a0e9be5411e15d60fb8fcde626afe25b
Ayhan <ayhan-games@hotmail.com>
2025-05-22 00:04:02 +0300

    responsive design added

--- frontend/src/pages/PopularAlbumsPage.tsx
+++ frontend/src/pages/PopularAlbumsPage.tsx
@@ -18,20 +18,20 @@ const PopularAlbumsPage = () => {
 
   return (
     <div className="w-full h-[90%] bg-primary overflow-y-auto scrollbar-custom px-6 ">
-      <div className="text-white text-3xl font-bold mb-3">
+      <div className="text-white text-xl md:text-3xl font-bold mb-3">
         <h1>Popular Albums</h1>
       </div>
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 pb-16">
         {popularAlbums.map((item, index) => (
           <Album item={item} key={index} />
         ))}
       </div>
 
-      <div className="w-full flex justify-center mt-6">
+      <div className="w-full  flex justify-center pb-7">
         {loading ? (
           <>
-            <div className="flex  h-full">
+            <div className="flex h-full">
               <div className="w-12 h-9 border-4 border-t-4 border-t-blue-500 border-gray-300 rounded-full animate-spin"></div>
             </div>
           </>
